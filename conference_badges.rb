250b33bfa41c3834b8e9138977505774a4a42d40
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 03:02:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-speakers == ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator
   
 end
