b7e3db6e2c0f3ed3c280c39fcb1cff0fb641aad3
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 03:13:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,9 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
 def batch_badge_creator(speakers)
   speakers.each do |names|
     puts "Hello, my name is #{names}"
